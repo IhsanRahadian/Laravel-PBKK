d6c0b5cb5a2e8d28e98c4317c3336890b1968173
IhsanRahadian <ihsanrahadian8@gmail.com>
2024-09-25 11:39:51 +0700

    commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman About</title>
-</head>
-<body>
-    <h1>Halaman About</h1>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman About</h3>
+    <p>Name: {{ $name }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['name' => 'Ihsan Adhika Rahadian', 'title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
